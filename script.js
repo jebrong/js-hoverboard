7409aca6eb3b2dbbc1c527ef15a0e249b3c367b2
jebrong <92987206+jebrong@users.noreply.github.com>
2022-11-07 20:42:17 +0800

    add color change and square input

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
 const container = document.querySelector(".container");
+const buttons = document.querySelectorAll("button");
 
-const colors = ["#970C10", "#BFCAD0", "#474440", "#738580"];
+const input = document.querySelector("input");
 
-const squares = 500;
+const colors = ["#970C10", "#BFCAD0", "#474440", "#004369"];
+
+let squares = 500;
+
+let hoverColor = "white";
+
+input.addEventListener("change", (e) => {
+  squares = e.target.value;
+  createSqures();
+});
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (button.innerText === "Color 1") {
+      hoverColor = colors[0];
+    } else if (button.innerText === "Color 2") {
+      hoverColor = colors[1];
+    } else if (button.innerText === "Color 3") {
+      hoverColor = colors[2];
+    } else if (button.innerText === "Color 4") {
+      hoverColor = colors[3];
+    } else {
+      alert("wtf");
+    }
+  });
+});
 
 const setColor = (e) => {
-  e.style.backgroundColor = "black";
+  e.style.backgroundColor = hoverColor;
 };
 
 const removeColor = (e) => {
-  e.style.backgroundColor = "white";
+  e.style.backgroundColor = "orange";
 };
 
-for (i = 0; i < squares; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
+let createSqures = () => {
+  for (i = 0; i < squares; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
 
-  square.addEventListener("mouseover", () => setColor(square));
-  square.addEventListener("mouseout", () => removeColor(square));
+    square.addEventListener("mouseover", () => setColor(square));
+    square.addEventListener("mouseout", () => removeColor(square));
 
-  container.appendChild(square);
-}
+    container.appendChild(square);
+  }
+};
